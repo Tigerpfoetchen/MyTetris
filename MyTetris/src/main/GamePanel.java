330f965ff971b1a7e518b56e61fd27b95c459c48
Tigerpfoetchen <pascaladam91@googlemail.com>
2023-02-28 23:18:04 +0100

    GameTime implemented

--- MyTetris/src/main/GamePanel.java
+++ MyTetris/src/main/GamePanel.java
@@ -4,57 +4,82 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.time.DayOfWeek;
 
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel implements Runnable{
+public class GamePanel extends JPanel implements Runnable {
 
+	private static final long serialVersionUID = 1L;
 	final int originalTileSize = 16;
 	final int scale = 3;
-	
+
 	final int tileSize = originalTileSize * scale;
 	final int maxScreenCol = 16;
 	final int maxScreenRow = 16;
 	final int screenWidth = tileSize * maxScreenCol;
 	final int screenHeight = tileSize * maxScreenRow;
-	
+	final int FPS = 5;
+
 	Thread gameThread;
-	
+
 	public GamePanel() {
-		
+
 		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
 		this.setBackground(Color.black);
 		this.setDoubleBuffered(true);
-		
+
 	}
-	
+
 	public void startGameThread() {
 		gameThread = new Thread(this);
 		gameThread.start();
 	}
-	
+
 	public void run() {
-		
-		update();
-		repaint();
-		
+
+		double drawInterval = 1_000_000_000 / FPS;
+		double nextDrawTime = System.nanoTime() + drawInterval;
+
+		while (gameThread != null) {
+
+			update();
+
+			repaint();
+
+			try {
+				double remainingTime = nextDrawTime - System.nanoTime();
+				remainingTime = remainingTime / 1_000_000;
+
+				if (remainingTime < 0) {
+					remainingTime = 0;
+				}
+
+				Thread.sleep((long) remainingTime);
+				
+				nextDrawTime += drawInterval;
+
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
-	
+
 	public void update() {
-		
+
 	}
-	
+
 	public void paintComponent(Graphics g) {
-		
+
 		super.paintComponent(g);
-		
-		Graphics2D g2 = (Graphics2D)g;
-		
+
+		Graphics2D g2 = (Graphics2D) g;
+
 		g2.setColor(Color.white);
-		
+
 		g2.fillRect(100, 100, tileSize, tileSize);
-		
-		
+
 	}
-	
+
 }
